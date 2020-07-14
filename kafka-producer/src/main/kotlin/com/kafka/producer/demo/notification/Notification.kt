fade98ad192b036d14f226536901f86c685cfaf1
Vadzim Kavalkou <Vadzim_Kavalkou@epam.com>
2020-07-14 17:36:30 +0300

    Build updated

--- kafka-producer/src/main/kotlin/com/kafka/producer/demo/notification/Notification.kt
+++ kafka-producer/src/main/kotlin/com/kafka/producer/demo/notification/Notification.kt
@@ -37,7 +37,7 @@ data class NotificationEvent(
     val type: EventType?
 )
 
-@CrossOrigin(origins = ["http://localhost:80"])
+@CrossOrigin(origins = ["http://localhost:8082", "http://gateway:8082"])
 @RequestMapping("/api/notifications")
 interface Api {
 
